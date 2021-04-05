bee8c4814ae170a1f00dad9b7f8e00a09ce5ad9c
joaot <JDinis99@gmail.com>
2021-04-05 18:23:06 +0100

    num of products in pantry

--- src/main/kotlin/store/pengu/server/daos/PantryDao.kt
+++ src/main/kotlin/store/pengu/server/daos/PantryDao.kt
@@ -3,10 +3,7 @@ package store.pengu.server.daos
 import org.jooq.*
 import org.jooq.impl.DSL
 import org.jooq.types.ULong
-import store.pengu.server.data.Pantry
-import store.pengu.server.data.Pantry_x_User
-import store.pengu.server.data.ProductInPantry
-import store.pengu.server.data.Product_x_Pantry
+import store.pengu.server.data.*
 import store.pengu.server.db.pengustore.tables.Pantries
 import store.pengu.server.db.pengustore.tables.Pantries.PANTRIES
 import store.pengu.server.db.pengustore.tables.PantryXUser
@@ -18,16 +15,22 @@ class PantryDao(
 ) {
     private val dslContext = DSL.using(conf)
 
-    fun getPantries(create: DSLContext = dslContext): List<Pantry> {
+    fun getPantries(create: DSLContext = dslContext): List<Pantry_details> {
         return create.select()
             .from(PANTRIES)
             .fetch().map {
-                Pantry(
+                Pantry_details(
                     id = it[PANTRIES.PANTRY_ID].toLong(),
                     code = it[PANTRIES.CODE],
                     name = it[PANTRIES.NAME],
                     latitude = it[PANTRIES.LATITUDE].toFloat(),
-                    longitude = it[PANTRIES.LONGITUDE].toFloat()
+                    longitude = it[PANTRIES.LONGITUDE].toFloat(),
+                    product_num = create.fetchCount(
+                        DSL.select()
+                            .from(PANTRIES)
+                            .join(PRODUCT_X_PANTRY).using(PANTRIES.PANTRY_ID)
+                            .where(PANTRIES.PANTRY_ID.eq(it[PANTRIES.PANTRY_ID]))
+                    )
                 )
             }
 
@@ -49,8 +52,10 @@ class PantryDao(
     }
 
     fun addPantry(pantry: Pantry, create: DSLContext = dslContext): Boolean {
-        return create.insertInto(PANTRIES,
-                PANTRIES.CODE, PANTRIES.NAME, PANTRIES.LATITUDE, PANTRIES.LONGITUDE)
+        return create.insertInto(
+            PANTRIES,
+            PANTRIES.CODE, PANTRIES.NAME, PANTRIES.LATITUDE, PANTRIES.LONGITUDE
+        )
             .values(pantry.code, pantry.name, pantry.latitude.toDouble(), pantry.longitude.toDouble())
             .execute() == 1
     }
@@ -66,9 +71,19 @@ class PantryDao(
     }
 
     fun addProductToPantry(product_x_pantry: Product_x_Pantry, create: DSLContext = dslContext): Boolean {
-        return create.insertInto(PRODUCT_X_PANTRY,
-            PRODUCT_X_PANTRY.PRODUCT_ID, PRODUCT_X_PANTRY.PANTRY_ID, PRODUCT_X_PANTRY.WANT_QTY, PRODUCT_X_PANTRY.HAVE_QTY)
-            .values(product_x_pantry.product_id, product_x_pantry.pantry_id, product_x_pantry.want_qty, product_x_pantry.have_qty)
+        return create.insertInto(
+            PRODUCT_X_PANTRY,
+            PRODUCT_X_PANTRY.PRODUCT_ID,
+            PRODUCT_X_PANTRY.PANTRY_ID,
+            PRODUCT_X_PANTRY.WANT_QTY,
+            PRODUCT_X_PANTRY.HAVE_QTY
+        )
+            .values(
+                product_x_pantry.product_id,
+                product_x_pantry.pantry_id,
+                product_x_pantry.want_qty,
+                product_x_pantry.have_qty
+            )
             .execute() == 1
     }
 
@@ -94,7 +109,7 @@ class PantryDao(
             .execute() == 1
     }
 
-    fun getProductsInPantry (pantry_id: Long, create: DSLContext = dslContext): List<ProductInPantry> {
+    fun getProductsInPantry(pantry_id: Long, create: DSLContext = dslContext): List<ProductInPantry> {
         return create.select()
             .from(PANTRIES)
             .join(PRODUCT_X_PANTRY).using(PANTRIES.PANTRY_ID)
@@ -115,9 +130,10 @@ class PantryDao(
     }
 
     fun addPantryToUser(pantry_x_user: Pantry_x_User, create: DSLContext = dslContext): Boolean {
-        return  create.insertInto(
+        return create.insertInto(
             PantryXUser.PANTRY_X_USER,
-            PantryXUser.PANTRY_X_USER.PANTRY_ID, PantryXUser.PANTRY_X_USER.USER_ID)
+            PantryXUser.PANTRY_X_USER.PANTRY_ID, PantryXUser.PANTRY_X_USER.USER_ID
+        )
             .values(pantry_x_user.pantryId, pantry_x_user.userId)
             .execute() == 1
     }
