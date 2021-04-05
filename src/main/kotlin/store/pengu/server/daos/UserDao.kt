0f243f3f4017609a86110877ed1efc9d2cd5d1b3
TiagoFonseca99 <47889514+TiagoFonseca99@users.noreply.github.com>
2021-04-05 16:22:24 +0100

    Fixed Pantry Problems

--- src/main/kotlin/store/pengu/server/daos/UserDao.kt
+++ src/main/kotlin/store/pengu/server/daos/UserDao.kt
@@ -100,7 +100,7 @@ class UserDao(
 
     fun addPantryToUser(pantry_x_user: Pantry_x_User, create: DSLContext = dslContext): Boolean {
         return  create.insertInto(PANTRY_X_USER,
-            PANTRY_X_USER.USER_ID, PANTRY_X_USER.PANTRY_ID)
+            PANTRY_X_USER.PANTRY_ID, PANTRY_X_USER.USER_ID)
             .values(pantry_x_user.userId, pantry_x_user.pantryId)
             .execute() == 1
     }
--- src/main/kotlin/store/pengu/server/data/Pantry_x_User.kt
+++ src/main/kotlin/store/pengu/server/data/Pantry_x_User.kt
@@ -1,6 +1,6 @@
 package store.pengu.server.data
 
 data class Pantry_x_User(
-    val userId: Long,
-    val pantryId: Long
+    val pantryId: Long,
+    val userId: Long
 )
